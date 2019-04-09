5f59a25c0f00b2a9df5c0d060041d5c8986ef2f0
Enrico Tassi <Enrico.Tassi@Inria.fr>
2019-04-09 17:15:46 +0200

    mock up declare_structure and declare_factory

--- demo.v
+++ demo.v
@@ -14,23 +14,29 @@ Record mixin_of A := Mixin {
   _ : laws A zero plus;
   }.
 
-Record class_of A := Class {
+Record class_of (A : Type) := Class {
   mixin : mixin_of A
   }.
 
 Structure type := Pack {
-  sort :> Type;
+  sort : Type;
   class : class_of sort
   }.
 
-End ASG. 
+Notation Make T m := (Pack T (Class _ m)).
 
-Notation AsgType T m := (ASG.Pack T (ASG.Class _ m)).
+Module Exports.
 
-Coercion ASG.sort : ASG.type >-> Sortclass.
+Coercion sort : type >-> Sortclass.
 
-Definition plus {A : ASG.type} := ASG.plus _ (ASG.mixin _ (ASG.class A)).
-Definition zero {A : ASG.type} := ASG.zero _ (ASG.mixin _ (ASG.class A)).
+Definition plus {A : type} := plus _ (mixin _ (class A)).
+Definition zero {A : type} := zero _ (mixin _ (class A)).
+
+End Exports.
+
+End ASG.
+Export ASG.Exports.
+
 Check fun x : _ => plus x zero = x. (* _ is a ASG.type *)
 
 Module RING.
@@ -43,46 +49,52 @@ Record mixin_of (A : ASG.type) := Mixin {
   _ : from_asg_laws A one times;
   }.
 
-Record class_of A := Class {
+Record class_of (A : Type) := Class {
   base : ASG.class_of A;
   mixin : mixin_of (ASG.Pack A base)
   }.
 
 Structure type := Pack {
-  sort :> Type;
+  sort : Type;
   class : class_of sort
   }.
 
-Definition asgType (R : type) : ASG.type := ASG.Pack R (base R (class R)).
 
 Definition pack (T : Type) (asg : ASG.type) (m : mixin_of asg) :=
   fun b of phant_id (ASG.class asg) b =>
   fun m' of phant_id m m' =>
     Pack T (Class _ b m').
 
+Notation Make T m := (pack T _ m _ idfun _ idfun).
+
+Module Exports.
+
+Definition times {A : type} := times _ (mixin _ (class A)).
+Definition one {A : type} := one _ (mixin _ (class A)).
+
+Coercion sort : type >-> Sortclass.
+
+Definition asgType (R : type) : ASG.type := ASG.Pack R (base R (class R)).
+
+Coercion asgType : type >-> ASG.type.
+Canonical asgType. (* RING.sort ? = ASG.sort ? *)
+
+End Exports.
+
 End RING.
 
-Notation RingType T M := (RING.pack T _ M _ idfun _ idfun).
-
-Definition times {A : RING.type} := RING.times _ (RING.mixin _ (RING.class A)).
-Definition one {A : RING.type} := RING.one _ (RING.mixin _ (RING.class A)).
-
-Coercion RING.sort : RING.type >-> Sortclass.
+Export RING.Exports.
 
 Check fun x : _ => times x one = x. (* _ is a RING.type *)
 
-Fail Check fun (r : RING.type) (x : r) => plus x one = x.
-
-Coercion RING.asgType : RING.type >-> ASG.type.
-Canonical RING.asgType. (* RING.sort ? = ASG.sort ? *)
-
+(* requires the Canonical asgType. *)
 Check fun (r : RING.type) (x : r) => plus x one = x. (* x is both in a ring and a group *)
 
 Axiom N_asg : ASG.laws nat 0 Nat.add.
-Canonical NasgType := AsgType nat (ASG.Mixin _ _ _ N_asg).
+Canonical NasgType := ASG.Make nat (ASG.Mixin _ _ _ N_asg).
 
 Axiom N_ring : RING.from_asg_laws _ 1 Nat.mul.
-Canonical NringType := RingType nat (RING.Mixin _ _ _ N_ring).
+Canonical NringType := RING.Make nat (RING.Mixin _ _ _ N_ring).
 
 Check fun n : nat => plus 1 (times 0 n) = n.
 
@@ -97,7 +109,7 @@ Module ASG_input.
 
 Axiom laws : forall T, T -> (T -> T -> T) -> Prop.
 
-Record mixin_of A := Mixin {
+Record from_type A := FromType { (* from scratch *)
   zero : A;
   plus : A -> A -> A;
   _ : laws A zero plus;
@@ -105,12 +117,12 @@ Record mixin_of A := Mixin {
 
 End ASG_input. 
 
-(* declare_structure ASG.mixin_of *)
+(* declare_structure ASG_input.mixin_of *)
 
 Module ASG.
 
-Record class_of A := Class {
-  mixin : ASG_input.mixin_of A (* TODO: inline *)
+Record class_of (A : Type) := Class {
+  mixin : ASG_input.from_type A (* TODO: inline *)
   }.
 
 Structure type := Pack {
@@ -118,7 +130,7 @@ Structure type := Pack {
   class : class_of sort
   }.
 
-Notation Make T m := (Pack T (Class _ m)).
+
 
 Module Exports.
 
@@ -130,66 +142,97 @@ Definition zero {A : type} := ASG_input.zero _ (mixin _ (class A)).
 End Exports.
 
 End ASG.
+
+(* declare_factory ASG_input.from_type *)
+
+Module ASG_Make.
+
+  Notation from_type T m := (ASG.Pack T (ASG.Class _ m)).
+
+End ASG_Make.
+
 Export ASG.Exports.
 
 (* test *)
 Check fun x : _ => plus x zero = x. (* _ is a ASG.type *)
 
-Module RING.
+Module RING_input.
 
 Axiom from_asg_laws : forall T : ASG.type, T -> (T -> T -> T) -> Prop.
 
-Record mixin_of (A : ASG.type) := Mixin {
+Record from_asg (A : ASG.type) := FromAsg {
   one : A;
   times : A -> A -> A;
   _ : from_asg_laws A one times;
   }.
 
-Record class_of A := Class {
+End RING_input.
+
+(* declare_structure base: ASG mix: RING_input.from_asg *)
+
+Module RING.
+
+Record class_of (A : Type) := Class {
   base : ASG.class_of A;
-  mixin : mixin_of (ASG.Pack A base)
+  mixin : RING_input.from_asg (ASG.Pack A base)
   }.
 
 Structure type := Pack {
-  sort :> Type;
+  sort : Type;
   class : class_of sort
   }.
 
+
+
+
+Module Exports.
+
+Definition times {A : type} := RING_input.times _ (mixin _ (class A)).
+Definition one {A : type} := RING_input.one _ (mixin _ (class A)).
+
+Coercion sort : type >-> Sortclass.
+
 Definition asgType (R : type) : ASG.type := ASG.Pack R (base R (class R)).
 
-Definition pack (T : Type) (asg : ASG.type) (m : mixin_of asg) :=
-  fun b of phant_id (ASG.class asg) b =>
-  fun m' of phant_id m m' =>
-    Pack T (Class _ b m').
+Coercion asgType : type >-> ASG.type.
+Canonical asgType. (* RING.sort ? = ASG.sort ? *)
+
+End Exports.
 
 End RING.
 
-Notation RingType T M := (RING.pack T _ M _ idfun _ idfun).
+Export RING.Exports.
 
-Definition times {A : RING.type} := RING.times _ (RING.mixin _ (RING.class A)).
-Definition one {A : RING.type} := RING.one _ (RING.mixin _ (RING.class A)).
+(* declare_factory RING_input.from_asg *)
 
-Coercion RING.sort : RING.type >-> Sortclass.
+Module RING_Make.
+
+Definition pack_from_asg (T : Type) (asg : ASG.type) (m : RING_input.from_asg asg) :=
+  fun asg' of phant_id (ASG.sort asg') T => (* (T : Type) = (sort asg' : Type)                *)
+  fun b' of phant_id (ASG.class asg') b' => (* (b' : class_of T)  = (class asg' : class_of T) *)
+  fun m' of phant_id m m' =>                (* (m' : from_asg asg') = (m : from_asg asg)
+                                               because the C provided in the type of (m : frmo_asg C)
+                                               may not be the canonical one, so we unify m and m' hence,
+                                               it will unify their types that contain asg and asg' *)
+    RING.Pack T (RING.Class _ b' m').
+
+Notation from_asg T m := (pack_from_asg T _ m _ idfun _ idfun _ idfun).
+
+End RING_Make.
+
 
 Check fun x : _ => times x one = x. (* _ is a RING.type *)
 
-Fail Check fun (r : RING.type) (x : r) => plus x one = x.
-
-Coercion RING.asgType : RING.type >-> ASG.type.
-Canonical RING.asgType. (* RING.sort ? = ASG.sort ? *)
-
 Check fun (r : RING.type) (x : r) => plus x one = x. (* x is both in a ring and a group *)
 
-Axiom N_asg : ASG.laws nat 0 Nat.add.
-Canonical NasgType := AsgType nat (ASG.Mixin _ _ _ N_asg).
+Axiom N_asg : ASG_input.laws nat 0 Nat.add.
+Canonical NasgType := ASG_Make.from_type nat (ASG_input.FromType _ _ _ N_asg).
 
-Axiom N_ring : RING.from_asg_laws _ 1 Nat.mul.
-Canonical NringType := RingType nat (RING.Mixin _ _ _ N_ring).
+Axiom N_ring : RING_input.from_asg_laws _ 1 Nat.mul.
+Canonical NringType := RING_Make.from_asg nat (RING_input.FromAsg _ _ _ N_ring).
 
 Check fun n : nat => plus 1 (times 0 n) = n.
 
-Check RingType
-
-End Example1.
+End Example1_meta.
 
 
