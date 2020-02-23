8abe07f029a43919c061b41b1f45e5c2fcc950f9
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-02-23 16:22:55 +0100

    some progress in demo2

--- demo2/stage10.v
+++ demo2/stage10.v
@@ -10,20 +10,18 @@ Local Open Scope hb_scope.
 
 Module Stage10.
 
-Elpi hb.structure TYPE.
+HB.structure TYPE.
 
-Elpi hb.declare_mixin AddAG_of_TYPE A.
-  Record axioms := Axioms {
-    zero : A;
-    add : A -> A -> A;
-    opp : A -> A;
-    addrA : associative add;
-    addrC : commutative add;
-    add0r : left_id zero add;
-    addNr : left_inverse zero opp add;
-  }.
-Elpi hb.end.
-Elpi hb.structure AddAG AddAG_of_TYPE.axioms.
+HB.mixin Record AddAG_of_TYPE A := {
+  zero : A;
+  add : A -> A -> A;
+  opp : A -> A;
+  addrA : associative add;
+  addrC : commutative add;
+  add0r : left_id zero add;
+  addNr : left_inverse zero opp add;
+}.
+HB.structure AddAG AddAG_of_TYPE.axioms.
 
 Notation "0" := zero : hb_scope.
 Infix "+" := (@add _) : hb_scope.
@@ -80,79 +78,74 @@ Proof. by rewrite opprD opprK addrC. Qed.
 
 End AddAGTheory.
 
-Elpi hb.declare_mixin Ring_of_AddAG A AddAG.axioms.
-  Record axioms := Axioms {
-    one : A;
-    mul : A -> A -> A;
-    mulrA : associative mul;
-    mulr1 : left_id one mul;
-    mul1r : right_id one mul;
-    mulrDl : left_distributive mul add;
-    mulrDr : right_distributive mul add;
-  }.
-Elpi hb.end.
-Elpi hb.declare_factory Ring_of_TYPE A.
-  Record axioms := Axioms {
-    zero : A;
-    one : A;
-    add : A -> A -> A;
-    opp : A -> A;
-    mul : A -> A -> A;
-    addrA : associative add;
-    addrC : commutative add;
-    add0r : left_id zero add;
-    addNr : left_inverse zero opp add;
-    mulrA : associative mul;
-    mul1r : left_id one mul;
-    mulr1 : right_id one mul;
-    mulrDl : left_distributive mul add;
-    mulrDr : right_distributive mul add;
-  }.
+HB.mixin Record Ring_of_AddAG A of AddAG.axioms A := {
+  one : A;
+  mul : A -> A -> A;
+  mulrA : associative mul;
+  mulr1 : left_id one mul;
+  mul1r : right_id one mul;
+  mulrDl : left_distributive mul add;
+  mulrDr : right_distributive mul add;
+}.
+HB.factory Record Ring_of_TYPE A := {
+  zero : A;
+  one : A;
+  add : A -> A -> A;
+  opp : A -> A;
+  mul : A -> A -> A;
+  addrA : associative add;
+  addrC : commutative add;
+  add0r : left_id zero add;
+  addNr : left_inverse zero opp add;
+  mulrA : associative mul;
+  mul1r : left_id one mul;
+  mulr1 : right_id one mul;
+  mulrDl : left_distributive mul add;
+  mulrDr : right_distributive mul add;
+}.
 
-  Variable a : axioms.
-  Definition to_AddAG_of_TYPE := AddAG_of_TYPE.Axioms_ A
-    _ _ _ (addrA a) (addrC a) (add0r a) (addNr a).
-  Elpi hb.canonical A to_AddAG_of_TYPE.
+HB.builders Context A (a : Ring_of_TYPE.axioms A).
+
+Definition to_AddAG_of_TYPE := AddAG_of_TYPE.Axioms A
+    _ _ _ addrA_a addrC_a add0r_a addNr_a.
+  HB.instance A to_AddAG_of_TYPE.
   Definition to_Ring_of_AddAG :=
-    Ring_of_AddAG.Axioms _ _ (mulrA a) (mul1r a)
-      (mulr1 a)
-      (mulrDl a : left_distributive _ (@AddAG.Exports.add _)) (mulrDr a).
-  Elpi hb.canonical A to_Ring_of_AddAG.
-Elpi hb.end.
+    Ring_of_AddAG.Axioms _ _ _ mulrA_a mul1r_a
+      mulr1_a mulrDl_a mulrDr_a.
+  HB.instance A to_Ring_of_AddAG.
+HB.end.
 
-Elpi hb.structure Ring Ring_of_TYPE.axioms.
+HB.structure Ring Ring_of_TYPE.axioms.
 
 Notation "1" := one : hb_scope.
 Infix "*" := (@mul _) : hb_scope.
 
-Elpi hb.declare_mixin Topological T.
-Record axioms := Axioms {
+HB.mixin Record Topological T := {
   open : (T -> Prop) -> Prop;
   open_setT : open setT;
   open_bigcup : forall {I} (D : set I) (F : I -> set T),
     (forall i, D i -> open (F i)) -> open (\bigcup_(i in D) F i);
   open_setI : forall X Y : set T, open X -> open Y -> open (setI X Y);
-  }.
-Elpi hb.end.
-Elpi hb.structure TopologicalSpace Topological.axioms.
+}.
+HB.structure TopologicalSpace Topological.axioms.
 
 Hint Extern 0 (open setT) => now apply: open_setT : core.
 
-Elpi hb.declare_factory TopologicalBase T.
-  Record axioms := Axioms {
-    open_base : set (set T);
-    open_base_covers : setT `<=` \bigcup_(X in open_base) X;
-    open_base_cup : forall X Y : set T, open_base X -> open_base Y ->
-      forall z, (X `&` Y) z -> exists2 Z, open_base Z & Z z /\ Z `<=` X `&` Y
-  }.
-  Variable a : axioms.
+HB.factory Record TopologicalBase T := {
+  open_base : set (set T);
+  open_base_covers : setT `<=` \bigcup_(X in open_base) X;
+  open_base_cup : forall X Y : set T, open_base X -> open_base Y ->
+    forall z, (X `&` Y) z -> exists2 Z, open_base Z & Z z /\ Z `<=` X `&` Y
+}.
 
-  Definition open_of :=
-    [set A | exists2 D, D `<=` open_base a & A = \bigcup_(X in D) X].
+HB.builders Context T (a : TopologicalBase.axioms T).
+
+  Definition open_of : (T -> Prop) -> Prop :=
+    [set A | exists2 D, D `<=` open_base_a & A = \bigcup_(X in D) X].
 
   Lemma open_of_setT : open_of setT.  Proof.
-  exists (open_base a); rewrite // predeqE => x; split=> // _.
-  by apply: open_base_covers.
+  exists open_base_a; rewrite // predeqE => x; split=> // _.
+  by apply: open_base_covers_a.
   Qed.
 
   Lemma open_of_bigcup {I} (D : set I) (F : I -> set T) :
@@ -163,10 +156,10 @@ Elpi hb.declare_factory TopologicalBase T.
   Proof. Admitted.
 
   Definition to_Topological :=
-    Topological.Axioms _ open_of_setT (@open_of_bigcup) open_of_cap.
-  Elpi hb.canonical T to_Topological.
+    Topological.Axioms T _ open_of_setT (@open_of_bigcup) open_of_cap.
+  HB.instance T to_Topological.
 
-Elpi hb.end.
+HB.end.
 
 Section ProductTopology.
   Variables (T1 T2 : TopologicalSpace.type).
@@ -196,8 +189,8 @@ Section ProductTopology.
   Qed.
 
   Definition prod_topology :=
-    TopologicalBase.Axioms _ prod_open_base_covers prod_open_base_setU.
-  Elpi hb.canonical (TopologicalSpace.sort T1 * TopologicalSpace.sort T2)%type prod_topology.
+    TopologicalBase.Axioms (TopologicalSpace.sort T1 * TopologicalSpace.sort T2)%type _ prod_open_base_covers prod_open_base_setU.
+  HB.instance (TopologicalSpace.sort T1 * TopologicalSpace.sort T2)%type prod_topology.
 
 End ProductTopology.
 
@@ -207,15 +200,12 @@ Definition continuous {T T' : TopologicalSpace.type} (f : T -> T') :=
 Definition continuous2 {T T' T'': TopologicalSpace.type}
   (f : T -> T' -> T'') := continuous (fun xy => f xy.1 xy.2).
 
-Elpi hb.declare_mixin JoinTAddAG
-  T AddAG_of_TYPE.axioms Topological.axioms.
-  Record axioms := Axioms {
-    add_continuous : continuous2 (add : T -> T -> T);
-    opp_continuous : continuous (opp : T -> T)
-  }.
-Elpi hb.end.
+HB.mixin Record JoinTAddAG T of AddAG_of_TYPE.axioms T & Topological.axioms T := {
+  add_continuous : continuous2 (add : T -> T -> T);
+  opp_continuous : continuous (opp : T -> T)
+}.
 
-Elpi hb.structure TAddAG
+HB.structure TAddAG
   Topological.axioms AddAG_of_TYPE.axioms
   JoinTAddAG.axioms.
 
@@ -226,7 +216,7 @@ Definition Z_ring_axioms :=
     Z.add_assoc Z.add_comm Z.add_0_l Z.add_opp_diag_l
     Z.mul_assoc Z.mul_1_l Z.mul_1_r
     Z.mul_add_distr_r Z.mul_add_distr_l.
-Elpi hb.canonical Z Z_ring_axioms.
+HB.instance Z Z_ring_axioms.
 
 Example test1 (m n : Z) : (m + n) - n + 0 = m.
 Proof. by rewrite addrK addr0. Qed.
@@ -242,12 +232,12 @@ Definition Qc_ring_axioms :=
     Qcplus_assoc Qcplus_comm Qcplus_0_l Qcplus_opp_l
     Qcmult_assoc Qcmult_1_l Qcmult_1_r
     Qcmult_plus_distr_l Qcmult_plus_distr_r.
-Elpi hb.canonical Qc Qc_ring_axioms.
+HB.instance Qc Qc_ring_axioms.
 
 Obligation Tactic := idtac.
 Definition Qcopen_base : set (set Qc) := 
   [set A | exists a b : Qc, forall z, A z <-> a < z /\ z < b].
-Program Definition QcTopological := TopologicalBase.Axioms_ Qc Qcopen_base _ _.
+Program Definition QcTopological := TopologicalBase.Axioms Qc Qcopen_base _ _.
 Next Obligation.
 move=> x _; exists [set y | x - 1 < y < x + 1].
   by exists (x - 1), (x + 1).
@@ -258,11 +248,11 @@ Qed.
 Next Obligation.
 move=> X Y [aX [bX Xeq]] [aY [bY Yeq]] z [/Xeq [aXz zbX] /Yeq [aYz zbY]].
 Admitted.
-Elpi hb.canonical Qc QcTopological.
+HB.instance Qc QcTopological.
 
-Program Definition QcJoinTAddAG := JoinTAddAG.Axioms_ Qc _ _.
+Program Definition QcJoinTAddAG := JoinTAddAG.Axioms Qc _ _.
 Next Obligation. Admitted.
 Next Obligation. Admitted.
-Elpi hb.canonical Qc QcJoinTAddAG.
+HB.instance Qc QcJoinTAddAG.
 
 End Stage10.
