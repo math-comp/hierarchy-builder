f3f877a2e44aa2b3300f3f76fa769ff70e5f29e4
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-03-05 15:30:41 +0100

    beta reduce the types of exported axioms (fix #50)

---
+++ tests/type_of_exported_ops.v
@@ -0,0 +1,20 @@
+From HB Require Import structures.
+
+Definition comb A op := forall x : A, op (op x) = x.
+
+HB.mixin Record Foo A := {
+  op : A -> A;
+  ax : comb A op
+}.
+
+HB.structure S := Foo.axioms.
+
+Set Printing All.
+Lemma test1 : True.
+Proof.
+pose proof @ax as H.
+match goal with
+| H : forall x : S.type, comb (S.sort x) op |- _ => trivial
+| H : ?T |- _ => fail "type of ax not as nice as expected:" T
+end.
+Qed.
