2dc9555f7c708a0314130b8fa80d2ec5fcc640b3
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-06-02 10:44:47 +0200

    test HB.instance with parameters (only in a section)

--- demo4/hierarchy_0.v
+++ demo4/hierarchy_0.v
@@ -9,6 +9,15 @@ HB.structure Definition s1 T := { A of m1 T A }.
 Check inhab.
   (* inhab : forall (T : Type) (A : s1.type T), s1.sort A *)
 
+HB.instance Definition nat_m1 := m1.Build bool nat 7 false.
+Check (refl_equal _ : @inhab _ _ = 7).
+
+Section Foo.
+Variable A : Type.
+HB.instance Definition list_m1 := m1.Build (option A) (list nat) (cons 7 nil) None.
+End Foo.
+Check (refl_equal _ : @inhab _ _ = (cons 7 nil)).
+
 HB.mixin Record m2 (T : Type) (A : Type) of m1 T A := {
   inj : T -> A;
 }.
