52dcc0484800e643262d1585920350243cf3ae40
Cyril Cohen <cohen@crans.org>
2021-02-15 18:37:03 +0100

    removing comment

--- default.nix
+++ default.nix
@@ -1,5 +1,3 @@
-# This file was generated from `meta.yml`, please do not edit manually.
-# Follow the instructions on https://github.com/coq-community/templates to regenerate.
 { config ? {}, withEmacs ? false, print-env ? false, do-nothing ? false,
   update-nixpkgs ? false, ci ? false, ci-step ? null, inNixShell ? null
 }@args:
