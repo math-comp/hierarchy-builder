67419ec401b0e307a4cf1ee83b90191d1cad9963
Cyril Cohen <cohen@crans.org>
2021-02-19 01:31:26 +0100

    update nixpkgs

--- .nix/config.nix
+++ .nix/config.nix
@@ -1,7 +1,7 @@
 {
   format = "1.0.0";
   coq-attribute = "hierarchy-builder";
-  namespace = ".";
+  namespace = "HB";
   realpath = ".";
   select = "coq-8.13";
   inputs."coq-8.13".coqPackages = {
