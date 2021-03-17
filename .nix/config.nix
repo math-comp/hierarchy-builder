8fc8aca5f060306424b3f3ae895fb8cfb72f9505
Cyril Cohen <cohen@crans.org>
2021-03-17 18:48:59 +0100

    testing more versions

--- .nix/config.nix
+++ .nix/config.nix
@@ -2,13 +2,22 @@
   format = "1.0.0";
   attribute = "hierarchy-builder";
   default-task = "coq-8.13";
-  tasks."coq-8.13".coqPackages = {
-    coq.override.version = "8.13";
-    mathcomp.override.version = "hierarchy-builder";
-    mathcomp.job = false;
-    mathcomp-single.job = true;
-    hierarchy-builder-shim.job = true;
-    mathcomp-single-planB-src.job = true;
-    mathcomp-single-planB.job = true;
+  tasks = let common = {
+      mathcomp.override.version = "hierarchy-builder";
+      mathcomp.job = false;
+      mathcomp-single.job = true;
+      hierarchy-builder-shim.job = true;
+      mathcomp-single-planB-src.job = true;
+      mathcomp-single-planB.job = true;
+  }; in {
+    "coq-8.13".coqPackages = {
+      coq.override.version = "8.13";
+    } // common;
+    "coq-8.12".coqPackages = {
+      coq.override.version = "8.12";
+    } // common;
+    "coq-8.11".coqPackages = {
+      coq.override.version = "8.11";
+    };
   };
 }
