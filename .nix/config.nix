d87f64f002691c5fbfea5867f2ea267940eb0ffb
Cyril Cohen <CohenCyril@users.noreply.github.com>
2021-03-18 11:49:39 +0100

    Update config.nix

--- .nix/config.nix
+++ .nix/config.nix
@@ -6,18 +6,19 @@
       mathcomp.override.version = "hierarchy-builder";
       mathcomp.job = false;
       mathcomp-single.job = true;
-      hierarchy-builder-shim.job = true;
-      mathcomp-single-planB-src.job = true;
-      mathcomp-single-planB.job = true;
   }; in {
     "coq-8.13".coqPackages = {
       coq.override.version = "8.13";
+      hierarchy-builder-shim.job = true;
+      mathcomp-single-planB-src.job = true;
+      mathcomp-single-planB.job = true;
     } // common;
     "coq-8.12".coqPackages = {
       coq.override.version = "8.12";
     } // common;
     "coq-8.11".coqPackages = {
       coq.override.version = "8.11";
-    };
+      coq-elpi.override.version = "backports-811";
+    } // common;
   };
 }
