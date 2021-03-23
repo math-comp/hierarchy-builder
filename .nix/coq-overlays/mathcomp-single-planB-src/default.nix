a30fff4b5dab8fa393b6287bb469052ecfac099f
Cyril Cohen <cohen@crans.org>
2021-03-23 13:23:07 +0100

    updating overlays

--- .nix/coq-overlays/mathcomp-single-planB-src/default.nix
+++ .nix/coq-overlays/mathcomp-single-planB-src/default.nix
@@ -1,14 +1,9 @@
 { mathcomp, coq, coq-elpi, hierarchy-builder, version ? null }:
 (mathcomp.override {single = true;}).overrideAttrs (old: {
   name = "coq${coq.coq-version}-mathcomp-planB-src";
-  patchPhase = ''
-    sed -i '/STOP\./Q' mathcomp/ssreflect/order.v
-    echo "End Order." >> mathcomp/ssreflect/order.v
-  '';
   buildPhase = ''
     COQ_ELPI_ATTRIBUTES='hb(log(raw))' \
-    make -j$NIX_BUILD_CORES -C mathcomp only \
-      TGTS="fingroup/presentation.vo algebra/ssralg.vo ssreflect/order.vo"
+    make -j$NIX_BUILD_CORES -C mathcomp
     coq.hb patch `find . -name \*.v`
     make -C mathcomp clean
   '';
--- .nix/coq-overlays/mathcomp-single/default.nix
+++ .nix/coq-overlays/mathcomp-single/default.nix
@@ -1,14 +1,5 @@
 { mathcomp, coq-elpi, hierarchy-builder, version ? null }:
 (mathcomp.override {single = true;}).overrideAttrs (old: {
-  patchPhase = ''
-    sed -i '/STOP\./Q' mathcomp/ssreflect/order.v
-    echo "End Order." >> mathcomp/ssreflect/order.v
-  '';
-  buildPhase = ''
-    make -j$NIX_BUILD_CORES -C mathcomp only \
-      TGTS="fingroup/presentation.vo algebra/ssralg.vo ssreflect/order.vo"
-  '';
   propagatedBuildInputs = old.propagatedBuildInputs ++
                           [ coq-elpi hierarchy-builder ];
-  installPhase = "echo NO INSTALL";
 })
