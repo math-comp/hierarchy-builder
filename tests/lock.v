bba75c7761c4222882910c113e750f3c4caa9423
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-04-12 19:05:30 +0200

    Fix scope of notations

--- tests/lock.v
+++ tests/lock.v
@@ -8,8 +8,14 @@ HB.lock Definition bar : nat1 := 3.
 
 HB.lock Definition baz n : nat := 3 + n.
 
+Module Import X.
+
 Axiom bigbody : Type -> Type -> Type.
 Axiom bigop : forall R I : Type, R -> list I -> (I -> bigbody R I) -> R.
 
-HB.lock Definition xxx := bigop.
+HB.lock Definition big := bigop.
+
+End X.
+
+About big.
 
