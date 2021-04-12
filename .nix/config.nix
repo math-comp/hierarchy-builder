9b67cecdad8dfae7dfa6ef36a017288a6b712c1b
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-04-12 20:25:20 +0200

    CI on the branch

--- .nix/config.nix
+++ .nix/config.nix
@@ -3,7 +3,7 @@
   attribute = "hierarchy-builder";
   default-bundle = "coq-8.13";
   bundles = let common = {
-      mathcomp.override.version = "hierarchy-builder";
+      mathcomp.override.version = "hierarchy-builder-lock";
       mathcomp.job = false;
       mathcomp-single.job = true;
   }; in {
