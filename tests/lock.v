abdb24c501bc0e79aea2e0e4b6a67d46d477a2a9
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-04-12 17:35:47 +0200

    fix universes

--- tests/lock.v
+++ tests/lock.v
@@ -7,3 +7,9 @@ Definition nat1 := nat.
 HB.lock Definition bar : nat1 := 3.
 
 HB.lock Definition baz n : nat := 3 + n.
+
+Axiom bigbody : Type -> Type -> Type.
+Axiom bigop : forall R I : Type, R -> list I -> (I -> bigbody R I) -> R.
+
+HB.lock Definition xxx := bigop.
+
