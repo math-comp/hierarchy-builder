8df6e510a36dbea27594b637d4f59b9987fea75e
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-06-08 17:48:40 +0200

    fix exported op type cleanup (fix #247)

--- tests/type_of_exported_ops.v
+++ tests/type_of_exported_ops.v
@@ -18,3 +18,18 @@ match goal with
 | H : ?T |- _ => fail "type of ax not as nice as expected:" T
 end.
 Qed.
+
+HB.mixin Record HasMul T := {
+  mul : T -> T -> T;
+  mulC: forall x y : T, mul x y = mul y x;
+  mulA: forall x y z : T, mul x (mul y z) = mul (mul x y) z;
+}.
+HB.structure Definition Mul := { T of HasMul T }.
+Lemma test2 : True.
+Proof.
+pose proof @mulA as H.
+match goal with
+| H : forall s : Mul.type, forall x y z : Mul.sort s, mul x (mul y z) = mul (mul x y) z  |- _ => trivial
+| H : ?T |- _ => fail "type of mulA not as nice as expected:" T
+end.
+Qed.
