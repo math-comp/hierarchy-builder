2b4835d95b633bf284861f8bba99e62c7088bdc7
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-06-22 16:05:49 +0200

    override nix

--- .nix/config.nix
+++ .nix/config.nix
@@ -6,6 +6,7 @@
       mathcomp.override.version = "hierarchy-builder";
       mathcomp.job = false;
       mathcomp-single.job = true;
+      coq-elpi.override.version = "#257";
   }; in {
     "coq-8.13".coqPackages = {
       coq.override.version = "8.13";
