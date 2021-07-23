f1c340df3e73a62afa1367849ead32a711269f83
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-07-23 16:32:01 +0200

    HB.structure: refresh universes in coercions (fix #248)

--- tests/primitive_records.v
+++ tests/primitive_records.v
@@ -24,3 +24,23 @@ Elpi Query lp:{{
   coq.locate "A.type" (indt Ind),
   std.assert! (coq.env.record? Ind tt) "not primitive"
 }}.
+
+(* Issue #248 *)
+#[primitive]
+HB.mixin Record HasMul T := {
+  mul : T -> T -> T;
+  mulC: forall x y : T, mul x y = mul y x;
+  mulA: associative mul;
+}.
+
+#[primitive, primitive_class]
+HB.structure Definition Mul := { T of HasMul T }.
+
+#[primitive]
+HB.mixin Record HasSq T of Mul T := {
+  sq : T -> T;
+  pmul : forall x y, sq (mul x y) = mul (sq x) (sq y);
+}.
+#[primitive, primitive_class]
+HB.structure Definition Sq := { T of HasSq T & Mul T }.
+Check erefl : Sq.sort _ = Mul.sort _.
