7807ee69b8a3d85eb246c11c6363fc9f9edfb09f
Enrico Tassi <Enrico.Tassi@Inria.fr>
2022-05-11 14:10:09 +0200

    wip

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = https://github.com/CohenCyril/nixpkgs/archive/19bda4700f7a0c7c4292e17887b771b4ba03c25a.tar.gz;
+         sha256 = "0jfy2yiwq72vpqmijk26pvvzbiamhd93ycfal8jkych6xqkjhr1g";
+       }
