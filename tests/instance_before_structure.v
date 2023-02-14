1e511637367ec368abaddd3eb903ce778044a28d
thomas <thomas.portet@etu.univ-cotedazur.fr>
2023-02-14 16:04:28 +0100

    first try on modifying HB

--- tests/instance_before_structure.v
+++ tests/instance_before_structure.v
@@ -3,8 +3,8 @@ From HB Require Import structures.
 HB.mixin Record m1 T := { default1 : T }.
 
 HB.mixin Record m2 T := { default2 : T }.
-
-HB.structure Definition s1 := { T of m1 T }.
+#[verbose]
+HB.structure Definition s1  := { T of m1 T }.
 
 HB.instance Definition _ : m1 nat := m1.Build nat 1.
 
@@ -14,6 +14,7 @@ HB.about nat.
 HB.instance Definition _ : m2 nat := m2.Build nat 2.
 
 HB.about nat. (* only s1 on nat *)
+
 
 HB.structure Definition s2 := { T of m1 T & m2 T }.
 
