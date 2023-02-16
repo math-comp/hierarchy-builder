a832dbf33f3ffd13dc973048b7bb8b4c3453f5d3
thomas <thomas.portet@etu.univ-cotedazur.fr>
2023-02-16 10:41:59 +0100

    taking out duplicates for the function in #336 and including a test for it

---
+++ tests/findall_cs_type.v
@@ -0,0 +1,22 @@
+From HB Require Import structures.
+From elpi Require Import elpi.
+HB.mixin Record m1 T := { default1 : T }.
+HB.mixin Record m2 T := { default1 : T }.
+
+(* no types yet*)
+Elpi Query HB.instance lp:{{std.assert! (findall-cs-types []) "a type was found"}}.
+
+HB.instance Definition _ : m1 nat := m1.Build nat 1.
+HB.instance Definition _ : m2 nat := m2.Build nat 2.
+(*no duplicates *)
+Elpi Query HB.instance lp:{{
+    std.assert! (findall-cs-types [X]) "no type",
+    std.assert! (X = {{nat}}) "wrong type".
+}}.
+
+HB.instance Definition _ : m1 bool := m1.Build bool true.
+
+Elpi Query HB.instance lp:{{
+    std.assert! (findall-cs-types [X,Y]) "no type",
+    std.assert! (Y = {{bool}}) "wrong type".
+}}.
