050163a13ad4cf5defdea0080411d0cd2102a898
thomas <thomas.portet@etu.univ-cotedazur.fr>
2023-02-23 17:21:55 +0100

    working on undup function and starting to rewrite the test for it

--- tests/findall_cs_type.v
+++ tests/findall_cs_type.v
@@ -1,5 +1,6 @@
 From HB Require Import structures.
 From elpi Require Import elpi.
+
 HB.mixin Record m1 T := { default1 : T }.
 HB.mixin Record m2 T := { default1 : T }.
 
@@ -8,10 +9,18 @@ Elpi Query HB.instance lp:{{std.assert! (findall-cs-types []) "a type was found"
 
 HB.instance Definition _ : m1 nat := m1.Build nat 1.
 HB.instance Definition _ : m2 nat := m2.Build nat 2.
+
+HB.instance Definition _ : m1 bool := m1.Build bool true.
+
+HB.instance Definition _ : m1 bool := m1.Build bool true.
+HB.instance Definition _ : m1 nat := m1.Build nat 1.
+HB.instance Definition _ : m2 nat := m2.Build nat 2.
+
 (*no duplicates *)
 Elpi Query HB.instance lp:{{
-    std.assert! (findall-cs-types [X]) "no type",
-    std.assert! (X = {{nat}}) "wrong type".
+    undup-gref [m1,m2] L,
+    std.assert! (std.last L m2) "no type",
+    
 }}.
 
 HB.instance Definition _ : m1 bool := m1.Build bool true.
