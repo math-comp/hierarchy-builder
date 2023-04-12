cfc026c021ace2e93ad3af2253e04220ec654ea2
Thomas <thoportet@gmail.com>
2023-04-12 16:27:56 +0200

    more tests and fixing the instance parameters case

--- tests/unit/mixin_src_has_mixin_instance.v
+++ tests/unit/mixin_src_has_mixin_instance.v
@@ -14,10 +14,18 @@ HB.instance Definition nat_m2 : m2 nat := m2.Build nat 1.
 
 Elpi Query HB.instance lp:{{
 mixin-src->has-mixin-instance [] (mixin-src {{nat}} M1 {{nat_m1}}) Y,
-Y = has-mixin-instance (cs-gref N) M1 I1 [],
-  coq.locate "nat" N,
-  coq.locate "m1.phant_axioms" M1,
-  coq.locate "nat_m1" I1.
+Y = has-mixin-instance (cs-gref {{:gref nat}}) {{:gref m1.phant_axioms}} {{:gref nat_m1}} [].
+
+}}.
+
+Section Test.
+Variable X:s1.type.
+
+Elpi Query HB.instance lp:{{
+coq.env.section L,
+mixin-src->has-mixin-instance L (mixin-src {{list X}} M1 {{i1 X}}) Y,
+Y = has-mixin-instance (cs-gref {{:gref list}}) {{:gref m1.phant_axioms}} {{:gref i1}} [ff].
+
 }}.
 
 
