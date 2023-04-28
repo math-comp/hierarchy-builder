05c259b98a6b6683d2b36c0497a6a8c1f3d5bf26
Thomas <thoportet@gmail.com>
2023-04-28 11:25:21 +0200

    fixing a situation shown in two_hier test file
    are currently rebasing branch 'instance-before-structure' on '713d039'.

--- tests/unit/close_hole_term.v
+++ tests/unit/close_hole_term.v
@@ -4,8 +4,10 @@ From Coq Require Export Setoid.
 
 Elpi Query HB.instance lp:{{
     X = app [{{list}}, Y],
+    % X needs to be typechecked here to get rid of the hole for the type of Y
+    coq.typecheck X _ ok,
     instance.close-hole-term X Z,
-    std.assert! (Z =  {{fun x => list x}} ) "term badly closed"
+    std.assert! (Z = {{fun x => list x}}) "term badly closed"
 }}.
 
 Elpi Query HB.instance lp:{{
@@ -20,6 +22,8 @@ inj x y : S (f x) (f y) -> R x y.
 Elpi Query HB.structure lp:{{
     Y = {{Inj}}, %Inj has 5 implicit arguments
     enrich-type Y X,
+    % X needs to be typechecked here to get rid of the holes of the types of its arguments
+    coq.typecheck X _ ok,
     instance.close-hole-term X Z,
-    std.assert! (Z = {{ fun a b c d e => @Inj e d c b a}}) "term badly closed".
+    std.assert! (Z = {{ fun a b c d e => @Inj a b c d e }}) "term badly closed"
 }}.
