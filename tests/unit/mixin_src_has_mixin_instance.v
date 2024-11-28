9515dcd6a1dae0d059b92e59ad8439388ce6972f
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-11-28 17:02:18 +0100

    remove typechecking warnings

--- tests/unit/mixin_src_has_mixin_instance.v
+++ tests/unit/mixin_src_has_mixin_instance.v
@@ -13,7 +13,7 @@ HB.instance Definition nat_m2 : m2 nat := m2.Build nat 1.
 
 
 Elpi Query HB.instance lp:{{
-mixin-src->has-mixin-instance (mixin-src {{nat}} M1 {{nat_m1}}) Y,
+mixin-src->has-mixin-instance (mixin-src {{nat}} M1_ {{nat_m1}}) Y,
 Y = has-mixin-instance (cs-gref {{:gref nat}}) {{:gref m1.phant_axioms}} {{:gref nat_m1}}.
 
 }}.
@@ -22,7 +22,7 @@ Section Test.
 Variable X:s1.type.
 
 Elpi Query HB.instance lp:{{
-mixin-src->has-mixin-instance (mixin-src {{list X}} M1 {{i1 X}}) Y,
+mixin-src->has-mixin-instance (mixin-src {{list X}} M1_ {{i1 X}}) Y,
 Y = has-mixin-instance (cs-gref {{:gref list}}) {{:gref m1.phant_axioms}} {{:gref i1}}.
 
 }}.
