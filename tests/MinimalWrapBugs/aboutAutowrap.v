150745c776cfdf59936781e8dfd56e4b403028be
Enrico Tassi <Enrico.Tassi@Inria.fr>
2025-05-13 12:05:50 +0200

    cleanup

--- tests/MinimalWrapBugs/aboutAutowrap.v
+++ tests/MinimalWrapBugs/aboutAutowrap.v
@@ -22,11 +22,7 @@ HB.structure Definition QPointed := {T of hasPoint T & Q__on__Pointed_x T }.
 HB.about QPointed. *)
 
 HB.structure Definition QPointed := {T of hasPoint T & Q _ (@x T) }.
-
 (* BUG: HB.about fails on structure defined relying on autowrap *)
+
 HB.about QPointed.
 HB.about QPointed.type.
-(* Anomaly "Uncaught exception Failure("split_dirpath")."
-   Please report at http://coq.inria.fr/bugs/. *)
-
-Print wrapper_xx_op'_mwb_isAssoc.
