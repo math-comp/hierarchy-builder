fbceca1cf6e21a9011876b47095c118e1b8eb5b9
Enrico Tassi <Enrico.Tassi@Inria.fr>
2025-05-14 15:46:51 +0200

    buildersofwrap works but need cleanup

--- tests/MinimalWrapBugs/buildersofwrap.v
+++ tests/MinimalWrapBugs/buildersofwrap.v
@@ -19,21 +19,6 @@ HB.factory Record isQPointed T := {
   y : T;
   qy : True
 }.
-
-(* WORKAROUND *)
-  (* HB.builders Context T of isQPointed T.
-
-  HB.instance Definition _ := hasPoint.Build T y.
-
-  HB.instance Definition temp := Q.Build T y qy.
-  HB.instance Definition _ := Q__on__Pointed_x.Build T temp.
-
-  HB.end.
-
-  HB.status.
-  (* As expected, any one of the two following work*)
-  HB.structure Definition QPointed := {T of hasPoint T & Q__on__Pointed_x T }.
-  HB.structure Definition QPointed' := {T of isQPointed T}. *)
   
 HB.builders Context T of isQPointed T.
 
@@ -43,9 +28,4 @@ HB.instance Definition _ := Q.Build T x qy.
 
 HB.end.
 
-HB.status. (* BUG: The builder targetting Q__on__Pointed_x is missing  *)
-Fail HB.structure Definition QPointed' := {T of isQPointed T}.
-(* Structure buildersofwrap2_Pointed contains the same mixins as QPointed' *)
-
-(* Though, this keep working... *)
-HB.structure Definition QPointed := {T of hasPoint T & Q__on__Pointed_x T }.
+HB.structure Definition QPointed' := {T of isQPointed T}.
